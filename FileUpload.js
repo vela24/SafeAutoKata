768963e5600a18058c3a31801e4138cdf6b7ac77
vela24 <42483900+vela24@users.noreply.github.com>
2020-08-13 17:41:08 -0700

    Add files via upload

---
+++ FileUpload.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module('UploadApp', ['ngFileUpload'])
+
+app.controller('MyController', function ($scope, Upload, $timeout) {
+    $scope.collection = [{
+        name: "Mark",
+        date: new Date("03/12/2019"),
+        driver: "Tad",
+        trip: "12 miles"
+    }];
+    var makeNewData = function () {
+        $scope.selectedEntry = {
+            name: ""
+        };
+    $scope.uploadFiles = function (files) {
+        $scope.SelectedFiles = files;
+        if ($scope.SelectedFiles && $scope.SelectedFiles.length) {
+            Upload.upload({
+                url: '/Home/Upload',
+                data: {
+                    files: $scope.SelectedFiles
+
+                }
+            }).then(function (response) {
+                $timeout(function () {
+                    $scope.result = response.data;
+                })
+            }, function (response) {
+                if (response.status > 0) {
+                    var errormsg = response.status + ':' + response.data;
+                }
+            }, function (d) {
+                var element = angular.element(document.querySelector('#divprogress'));
+                $scope.Progress = Math.min(100, parseInt(100.0 * d.loaded / d.total));
+                element.html('<div style="width:' + $scope.Progress + '%">' + $scope.Progress + '%</div>');
+            });
+        }
+    }
+    })
+
---
+++ HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tester2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Upload()
+        {
+          
+            return View();
+        }
+        [HttpPost]
+        public ContentResult Upload(string str)
+        {
+            string path = Server.MapPath("~/Upload");
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            foreach (string key in Request.Files)
+            {
+                HttpPostedFileBase postedFile = Request.Files[key];
+                postedFile.SaveAs(path + postedFile.FileName);
+            }
+            return Content("Success");
+        }
+    }
+}
